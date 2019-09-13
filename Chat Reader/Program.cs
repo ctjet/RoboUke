380cf17c4cc76043cf82f29d4492b2e23ec93ee0
Brandon C <b.cline1@yahoo.com>
2019-09-13 18:13:19 -0500

    Initial commit

---
+++ Chat Reader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chat_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
