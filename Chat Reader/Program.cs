ae9afe7c6371dec827ad72847f72772771e943a7
Brandon C <b.cline1@yahoo.com>
2019-09-13 18:28:59 -0500

    yeet

--- Chat Reader/Program.cs
+++ Chat Reader/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace Chat_Reader
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace Chat_Reader {
+	class Program {
+		static void Main(string[] args) {
+			
+		}
+	}
 }
